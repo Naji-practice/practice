d8174224969044370df1ef2386531bf906b1149d
Naji-practice <vvnn6348@gmail.com>
2025-08-23 12:02:17 +0530

    new cg hanges

---
+++ clubs.py
@@ -0,0 +1,15 @@
+clubs = {
+    "Coding_club":(
+        ("01","naji"),
+        ("02","rahan"),
+        ("03","nivi"),
+        ("04","shamil")
+    ),
+    "Dance_club":(
+        ("07","agney"),
+        ("02","rahan"),
+        ("03","nivi"),
+        ("08","aswanth")
+    ),
+
+}
